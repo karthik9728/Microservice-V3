54bb2e0197df151b0ffc58c2ca31f368312c950d
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-07-11 08:03:44 +0530

    Sidebar UI

---
+++ AutoMobile.UI/AutoMobile.Web/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMobile.Web.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadPage(string pageName)
+        {
+            // Handle the logic to determine which page to load based on the 'pageName' parameter
+            // Render the corresponding partial view and return it
+            return PartialView($"_{pageName}"); // Assuming partial views are named with an underscore prefix (e.g., "_Home.cshtml")
+        }
+    }
+}
